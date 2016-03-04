5700b91ef9a32b0fea600b5b92ad232cf4fedfc9
Jiateng Wang <wjt1223@gmail.com>
2016-03-03 23:40:26 -0800

    update for code quality

--- controller/listController.js
+++ controller/listController.js
@@ -1,18 +1,20 @@
-var app = angular.module('myTodo', []); 
+/*global angular */
+var app = angular.module('myTodo', []);
 
-app.controller('todoCtrl', function($scope) {
-    $scope.todoList = [{todoText:'Clean House', done:false}];
+app.controller('todoCtrl', function ($scope) {
+    "use strict";
+    $scope.todoList = [{todoText: 'Clean House', done: false}];
 
-    $scope.todoAdd = function() {
-        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+    $scope.todoAdd = function () {
+        $scope.todoList.push({todoText: $scope.todoInput, done: false});
         $scope.todoInput = "";
     };
 
-    $scope.remove = function() {
+    $scope.remove = function () {
         var oldList = $scope.todoList;
         $scope.todoList = [];
-        angular.forEach(oldList, function(x) {
-            if (!x.done) $scope.todoList.push(x);
+        angular.forEach(oldList, function (x) {
+            if (!x.done) { $scope.todoList.push(x); }
         });
     };
 });
