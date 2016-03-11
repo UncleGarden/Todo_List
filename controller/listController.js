8d5af8a7d799d06cf686bde9852574eea4611c76
Jiateng Wang <wjt1223@gmail.com>
2016-03-10 16:57:05 -0800

    try to update/clean the code

--- controller/listController.js
+++ controller/listController.js
@@ -1,56 +1,102 @@
 /*global angular */
+/*global console */
 var app = angular.module('myTodo', []);
 
 app.controller('todoCtrl', function ($scope) {
     "use strict";
-    $scope.allList = [];
-    $scope.activeList = [];
-    $scope.completeList = [];
+    
+    var item = {todoText: '', done: false, status: 'active'};
+    $scope.notes = [];
+    
+//    
+//    $scope.allList = [];
+//    $scope.activeList = [];
+//    $scope.completeList = [];
     $scope.date = new Date();
 
-    
-    $scope.all = true;
-    $scope.active = false;
-    $scope.complete = false;
+    $scope.currentStatus = 'all';
+//    $scope.all = true;
+//    $scope.active = false;
+//    $scope.complete = false;
     
     $scope.get_all = function () {
-        $scope.all = true;
-        $scope.active = false;
-        $scope.complete = false;
+        $scope.currentStatus = 'all';
     };
     
     $scope.get_active = function () {
-        $scope.all = false;
-        $scope.active = true;
-        $scope.complete = false;
+        $scope.currentStatus = 'active';
     };
     
     $scope.get_complete = function () {
-        $scope.all = false;
-        $scope.active = false;
-        $scope.complete = true;
+        $scope.currentStatus = 'complete';
     };
+    
+//    $scope.get_all = function () {
+//        $scope.all = true;
+//        $scope.active = false;
+//        $scope.complete = false;
+//    };
+//    
+//    $scope.get_active = function () {
+//        $scope.all = false;
+//        $scope.active = true;
+//        $scope.complete = false;
+//    };
+//    
+//    $scope.get_complete = function () {
+//        $scope.all = false;
+//        $scope.active = false;
+//        $scope.complete = true;
+//    };
 
+//    $scope.add = function () {
+//        $scope.allList.push({todoText: $scope.todoInput, done: false});
+//        $scope.activeList.push({todoText: $scope.todoInput, done: false});
+//        $scope.todoInput = "";
+//    };
+    
     $scope.add = function () {
-        $scope.allList.push({todoText: $scope.todoInput, done: false});
-        $scope.activeList.push({todoText: $scope.todoInput, done: false});
-        $scope.todoInput = "";
+        var temp, index;
+        temp = angular.copy(item);
+        
+        temp.todoText = $scope.todoInput;
+        temp.done = false;
+        temp.status = 'active';
+        index = $scope.notes.indexOf(temp);
+        
+        if (index === -1) {
+            $scope.notes.push(temp);
+            $scope.todoInput = "";
+            console.info("Item added successfully : ", $scope.notes);
+        } else {
+            console.info("Item exist");
+        }
+        
+        
     };
 
-    $scope.remove = function () {
-        var oldList = $scope.allList;
-        
-        $scope.allList = [];
-        $scope.activeList = [];
-        $scope.completeList = [];
-        angular.forEach(oldList, function (x) {
-            if (!x.done) {$scope.allList.push(x);
-                          $scope.activeList.push(x);
-                         }
-            if (x.done) {$scope.allList.push(x);
-                          $scope.completeList.push(x);
-                         }
-        });
+//    $scope.remove = function () {
+//        var oldList = $scope.allList;
+//        
+//        $scope.allList = [];
+//        $scope.activeList = [];
+//        $scope.completeList = [];
+//        angular.forEach(oldList, function (x) {
+//            if (!x.done) {$scope.allList.push(x);
+//                          $scope.activeList.push(x);
+//                         }
+//            if (x.done) {$scope.allList.push(x);
+//                          $scope.completeList.push(x);
+//                         }
+//        });
+//    };
+    
+    
+    $scope.remove = function (removedItem) {
+        var index = $scope.notes.indexOf(removedItem);
+        if (index > -1) {
+            $scope.notes[index].status = 'complete';
+        }
     };
 });
 
